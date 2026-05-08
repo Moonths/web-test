5142ff6c13c94abe645d519aa8d63955d06c56bb
毛际可 <dk_maojike@dakai>
2026-05-08 17:58:06 +0800

    update: use domain and https

--- backend/main.py
+++ backend/main.py
@@ -64,6 +64,8 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
         "http://101.200.35.168",
+        "https://maojike.me",
+        "https://www.maojike.me",
     ],
     allow_credentials=True,
     allow_methods=["*"],
